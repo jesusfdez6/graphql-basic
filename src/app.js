b48c2feae6290b283ffe24d11adf9e5b30f272a0
jesusfdez6 <60519826+jesusfdez6@users.noreply.github.com>
2025-06-18 00:26:59 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
-
+//hola
 const routerApi = require('./routes');
 const { checkApiKey } = require('./middlewares/auth.handler');
 
